90ad9bac9b1ef24e0a054a23ae3356388933b7ba
Enghav <enghav12@gmail.com>
2023-07-13 20:03:37 +0700

    local

---
+++ local.js
@@ -0,0 +1,49 @@
+const taskForm = document.getElementById('taskForm');
+const taskList = document.getElementById('taskList');
+const taskTitleInput = document.getElementById('taskTitle');
+const assigneeInput = document.getElementById('assignee');
+const taskDateInput = document.getElementById('taskDate');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+function renderTasks() {
+  taskList.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement('tasks');
+    listItem.innerHTML = `
+      <div class="flex w-[700px] justify-between border-2 p-2 rounded-[10px] text-slate-700 ">
+        <span>${task.title}</span>
+        <span>${task.assignee}</span>
+        <span>${task.date}</span>
+        <button class="bg-red-500 hover:bg-red-700 text-white py-1 px-2 ml-2 rounded" onclick="deleteTask(${index})">Delete</button>
+      </div>
+    `;
+    taskList.appendChild(listItem);
+  });
+}
+function addTask(event) {
+  event.preventDefault();
+
+  const title = taskTitleInput.value;
+  const assignee = assigneeInput.value;
+  const date = taskDateInput.value;
+
+  const task = { title, assignee, date };
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  taskTitleInput.value = '';
+  assigneeInput.value = '';
+  taskDateInput.value = '';
+  renderTasks();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  renderTasks();
+}
+
+taskForm.addEventListener('submit', addTask);
+
+renderTasks();
